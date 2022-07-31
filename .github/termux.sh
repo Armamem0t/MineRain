95bccbd4308709b8c5965d02e8e11a7d727ce639
MingLi <109898949+Armamem0t@users.noreply.github.com>
2022-07-31 10:03:06 +0800

    Update termux.sh

--- .github/termux.sh
+++ .github/termux.sh
@@ -1,1 +1,8 @@
-su
+echo "开始下载MineRain"
+cd /storage/emulated/0/Android/data/com.mojang.minecraftpe/files/games/com.mojang/resource_packs/
+echo "_____ "
+echo "|_ _|  __  _ __ _ __ ___ _  ___ __"
+echo " | | / _ \ '__| '_ `_\| | | \ \/ /"
+echo " | | __ / | | | | | | | |_| | > <"
+echo " |_|\___|_| |_| |_| |_|\__,_/_/\_\"
+wget https://objects.githubusercontent.com/github-production-release-asset-2e65be/517286240/7e75c0d1-e434-4bc4-9898-90ff5a59dd5b?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20220731%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220731T015424Z&X-Amz-Expires=300&X-Amz-Signature=88782f04fb87ae2642cd67a6a53fd1f3ee739d08358f49bf5400fff6df6c3bc9&X-Amz-SignedHeaders=host&actor_id=109898949&key_id=0&repo_id=517286240&response-content-disposition=attachment%3B%20filename%3DMineRainUI.zip&response-content-type=application%2Foctet-stream
